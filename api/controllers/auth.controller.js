1fb2d94205b39d5bc04b25e6db3349e50e2bef0f
Ricardo Robles <rroblesgo@gmail.com>
2023-09-17 20:47:12 +0200

    add a middleware and a function to handle error

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,7 +1,7 @@
 import bcryptjs from 'bcryptjs';
 import User from '../models/user.model.js';
 
-export const signup = async (req, res) => {
+export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
   const salt = bcryptjs.genSaltSync(10);
   const hashedPassword = bcryptjs.hashSync(password, salt);
@@ -16,6 +16,8 @@ export const signup = async (req, res) => {
       message: 'User created successfully',
     });
   } catch (error) {
-    res.status(500).json({ message: error.message });
+    // res.status(500).json({ message: error.message });
+    // next(errorHandler(500, 'something went wrong'));
+    next(error);
   }
 };
--- api/index.js
+++ api/index.js
@@ -20,6 +20,17 @@ app.use(express.json());
 app.use('/api/user', userRoutes);
 app.use('/api/auth', authRoutes);
 
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || 'Internal Server Error';
+  return res.status(statusCode).json({
+    success: false,
+    message,
+    statusCode,
+    stack: process.env.NODE_ENV === 'production' ? null : err.stack,
+  });
+});
+
 app.listen(3000, () => {
   console.log('Server listening on port 3000');
 });
