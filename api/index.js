0897b4410cd841509ac877ab98c7ac2a498bdff9
Ricardo Robles <rroblesgo@gmail.com>
2023-09-17 09:26:21 +0200

    connect to de database

--- api/index.js
+++ api/index.js
@@ -1,6 +1,19 @@
+import dotenv from 'dotenv';
 import express from 'express';
+import mongoose from 'mongoose';
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
 
 const app = express();
+
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
